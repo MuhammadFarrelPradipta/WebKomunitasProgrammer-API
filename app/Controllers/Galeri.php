5e044da9527eba91ca338e72b533eb387c474665
unknown <muhammadfarrelpradipta@gmail.com>
2022-09-14 14:32:13 +0700

    Make API for table 'galeri'

--- app/Controllers/Galeri.php
+++ app/Controllers/Galeri.php
@@ -4,18 +4,84 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\API\ResponseTrait;
-use App\Models\ModelBerita;
+use App\Models\ModelGaleri;
+
 
 class Galeri extends BaseController
 {
     use ResponseTrait;
     function __construct()
     {
-        $this->model = new ModelBerita();
+        $this->model = new ModelGaleri();
     }
     public function index()
     {
-        $data = $this->model->orderBy('Judul_berita', 'asc')->findAll();
+        $data = $this->model->orderBy('Nama_foto', 'asc')->findAll();
         return $this->respond($data, 200);
+    }
+    public function show($id = null)
+    {
+        $data = $this->model->where('Id_foto', $id)->findAll;
+        if ($data) {
+            return $this->respond($data, 200);
+        } else {
+            return $this->failNotFound('Data tidak ditemukan untuk id = ' . $id);
+        }
+    }
+    public function create()
+    {
+        $data = $this->request->getPost();
+        if (!$this->model->save($data)) {
+            return $this->fail($this->model->errors());
+        }
+        $response = [
+            'status' => 201,
+            'error' => null,
+            'message' => [
+                'success' => 'Arigout Gozaimasu'
+            ]
+        ];
+        return $this->respond($response);
+    }
+    public function update($id = null)
+    {
+        $data = $this->request->getRawInput();
+        $data['Id_foto'] = $id;
+
+        $isExist = $this->model->where('Id_foto', $id)->findAll();
+        if (!$isExist) {
+            return $this->failNotFound('Data tidak ditemukan untuk id = ' . $id);
+        }
+
+        if (!$this->model->save($data)) {
+            return $this->fail($this->model->errors());
+        }
+
+        $response = [
+            'status' => 200,
+            'error' => null,
+            'messages' => [
+                'success' => "Data Galeri dengan id $id berhasil di update"
+            ]
+        ];
+        return $this->respond($response);
+    }
+
+    public function delete($id = null)
+    {
+        $data = $this->model->where('Id_foto', $id)->findAll();
+        if ($data) {
+            $this->model->delete($id);
+            $response = [
+                'status' => 200,
+                'error' => null,
+                'messages' => [
+                    'success' => "Data berita telah dihapus"
+                ]
+            ];
+            return $this->respondDeleted($response);
+        } else {
+            return $this->failNotFound('Data tidak ditemukan');
+        }
     }
 }
--- app/Models/ModelGaleri.php
+++ app/Models/ModelGaleri.php
@@ -1,7 +1,29 @@
 <?php
 
+namespace App\Models;
+
 use CodeIgniter\Model;
 
 class ModelGaleri extends Model
 {
+    protected $table = "galeri";
+    protected $primaryKey = "Id_foto";
+    protected $allowedFields = ['Nama_foto', 'Deskripsi_foto', 'Foto'];
+    protected $validationRules = [
+        'Nama_foto' => 'required',
+        'Deskripsi_foto' => 'required',
+        'Foto' => 'required'
+    ];
+    protected $validationMessages = [
+        'Nama_foto' => [
+            'required' => 'Silahkan masukan nama dari foto tersebut'
+        ],
+        'Deskripsi_foto' => [
+            'required' => 'Silahkan masukan deskripsi foto tersebut'
+        ],
+        'Foto' => [
+            'required' => 'Silahkan masukan foto'
+        ]
+
+    ];
 }
